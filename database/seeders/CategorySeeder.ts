b0bc2537b60e8f28a04da36cb0e829782a5bd9c3
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-09-18 17:05:19 +0200

    install and configure @adonisjs/auth

---
+++ database/seeders/CategorySeeder.ts
@@ -0,0 +1,8 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Category from 'App/Models/Category'
+
+export default class extends BaseSeeder {
+  public async run() {
+    await Category.createMany([{ name: 'General' }, { name: 'Adonis' }])
+  }
+}
