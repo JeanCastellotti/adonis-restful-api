67a2da9687fa4a0a205f1178c2988ba00fb854d1
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-09-20 18:41:08 +0200

    setup login and register

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,27 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import RegisterValidator from 'App/Validators/RegisterValidator'
+
+export default class AuthController {
+  public async register({ request, auth }: HttpContextContract) {
+    const payload = await request.validate(RegisterValidator)
+
+    const user = await User.create(payload)
+
+    const token = await auth.login(user)
+
+    return token
+  }
+
+  public async login({ request, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+
+    try {
+      const token = await auth.attempt(email, password)
+
+      return token
+    } catch (error) {
+      return "We couldn't verify your credentials"
+    }
+  }
+}
---
+++ app/Validators/RegisterValidator.ts
@@ -0,0 +1,17 @@
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class RegisterValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    username: schema.string(),
+    email: schema.string([
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email' }),
+    ]),
+    password: schema.string(),
+  })
+
+  public messages: CustomMessages = {}
+}
