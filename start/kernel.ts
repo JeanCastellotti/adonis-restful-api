6b563eddbff4864f981ed40303eab1a92de16593
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-09-21 09:39:51 +0200

    setup route model binding

--- start/kernel.ts
+++ start/kernel.ts
@@ -3,6 +3,7 @@ import Server from '@ioc:Adonis/Core/Server'
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
   () => import('App/Middleware/SilentAuth'),
+  () => import('@ioc:Adonis/Addons/RmbMiddleware'),
 ])
 
 Server.middleware.registerNamed({
